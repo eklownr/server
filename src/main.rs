3fd6def35b3de62186cae81d188f76b88eff0709
eklownr <eklownr@gmail.com>
2024-01-13 23:28:38 +0100

    Add TcpListener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{Error, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let listener: TcpListener =
+        TcpListener::bind("127.0.0.1:7878").unwrap(); 
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("Connection established!");
+
+                // Handle stream here
+            }
+            Err(err) => {
+                eprintln!("Error accepting connection: {}", err);
+            }
+        }
+    }
 }
